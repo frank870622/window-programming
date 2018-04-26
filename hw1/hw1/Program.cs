02166fb06b8b8586a03b17f1071b6be7a427b3f4
frank870622 <frank870622@gmail.com>
2018-04-26 14:46:42 +0800

    Add files via upload

---
+++ hw1/hw1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[] { };
+            int[] chinese = new int[] { };
+            int[] english = new int[] { };
+            int[] math = new int[] { };
+            while (true)
+            {
+                Console.Write("請選擇動作: 1)輸入資料 2)印出資料 3)排序");
+            }
+            Console.Write(number.Length);
+            Console.Read();
+        }
+    }
+}
