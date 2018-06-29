d649f91446e055fd906b7e354e1bbb031d6446e5
frank870622 <frank870622@gmail.com>
2018-06-29 21:11:58 +0800

    Add files via upload

---
+++ Practice 7.2/Practice 7.2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice_7._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<TabPage> pages = new List<TabPage>();
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pages.Add(pageLogin);
+            pages.Add(pageEnglish);
+            pages.Add(pageChinese);
+            //pageLogin.Parent = pageControl;
+            //pageEnglish.Parent = pageControl;
+            //pageChinese.Parent = pageControl;
+
+            pageControl.TabPages.Remove(pageChinese);
+            pageControl.TabPages.Remove(pageEnglish);
+
+            passwardBox.PasswordChar = '*';　
+        }
+        private void pageLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if(nameBox.Text == "0" && passwardBox.Text == "0")
+            {
+                pageControl.TabPages.Remove(pageLogin);
+                pageControl.TabPages.Add(pages[1]);
+                pageControl.TabPages.Add(pages[2]);
+            }
+            else
+            {
+                MessageBox.Show("帳號或密碼錯誤", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ Practice 9.1/Practice 9.1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Practice_9._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "please enter the seconds countdown:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(205, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(28, 99);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(277, 81);
+            this.progressBar.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Press the start button to start program";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(265, 210);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 288);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
